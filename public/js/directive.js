8e0b6147265c5748d5182da0f2d5ffc3a48c4516
Jeff Yu <jeff.yu@sparkventures.co.nz>
2016-10-06 11:20:23 +1300

    remove debug code..

--- public/js/directive.js
+++ public/js/directive.js
@@ -6,11 +6,11 @@ app.directive('reOrder', function ($timeout) {
         link: function (scope, element, attrs) {
             element.on('focusout', function(event) {
             	var val = element.val();
-            	console.log(val);
+
 	        	if (val && val > 0 && val <= scope.content.clone.length) {
 		            var oldIndex = _.indexOf(scope.content.clone, scope.content.editValue);
 		            var oldData = scope.content.clone[oldIndex];
-		            console.log(oldIndex, oldData);
+
 		            if (oldIndex+1 != val) {
 		                scope.content.clone = _.without(scope.content.clone, scope.content.editValue);
 		                scope.content.clone.splice(val-1, 0, oldData);
